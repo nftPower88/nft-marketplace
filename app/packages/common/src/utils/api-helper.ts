28e62f7c5a45699edf907d9c50fd758b2b4422fa
codextech <tanzeelsaleem10@gmail.com>
2022-01-31 01:24:35 +0500

    api integrated for getting user info from db

---
+++ app/packages/common/src/utils/api-helper.ts
@@ -0,0 +1,8 @@
+export async function fetchGetJSON(url: string) {
+    try {
+      const data = await fetch(url).then(res => res.json());
+      return data;
+    } catch (err: any) {
+      throw new Error(err.message);
+    }
+  }
