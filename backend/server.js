0d24c51ebaa471f28f1808eebacd31a6eb5edd5c
codextech <tanzeelsaleem10@gmail.com>
2022-02-02 15:57:26 +0500

    update docker-compose fiel to connect db

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,9 @@ db.mongoose
     console.log("Connected to the database!");
   })
   .catch(err => {
-    console.log("Cannot connect to the database!", err);
+    console.log(db.connString , " Cannot connect to the database!", err);
     process.exit();
   });
-
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome !" });
@@ -38,5 +37,5 @@ require("./routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}. http://localhost:${PORT}`);
 });
